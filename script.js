e17e43e1f711765c1d590a9a7c250138ebd9b719
Natalia Derbeneva <nat_ff@mail.ru>
2020-03-12 22:20:26 +0300

    feat: make switching menu in header

---
+++ script.js
@@ -0,0 +1,19 @@
+const headerNav = document.querySelector('.header__navigation');
+
+headerNav.addEventListener('mousedown',select)
+
+
+function select(event){
+   // 
+   if(event.target.tagName == "A"){
+       let navListLength = headerNav.children[0].children.length;
+       let navList = headerNav.children[0].children;
+
+       for(let i=0;i<navListLength; i+=2){
+         navList[i].children[0].classList.remove('header__navigation_selected');
+       }
+       
+       event.target.classList.add('header__navigation_selected');
+   }
+    
+}
