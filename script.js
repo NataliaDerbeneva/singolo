1a3b31fb59875ad6bc4cba91ade3ee5bea99f2f4
Natalia Derbeneva <nat_ff@mail.ru>
2020-03-14 05:05:48 +0300

    feat: make slide switching

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const headerNav = document.querySelector('.header__navigation');
 
-headerNav.addEventListener('mousedown',select)
+headerNav.addEventListener('click',select)
 
 
 function select(event){
@@ -15,4 +15,25 @@ function select(event){
        
        event.target.classList.add('header__navigation_selected');
    }
+}
+
+const arrows = document.querySelectorAll('.arrow-link');
+arrows.forEach(arrow => arrow.addEventListener('click', slideList));
+const slides = arrows[0].parentElement.querySelectorAll('.slide');
+let slideOrder = 0;
+
+function slideList (event){
+
+  event.preventDefault();
+  if(slideOrder == 0){
+    slideOrder = 1;
+    slides[0].classList.add('slide-list');
+    document.querySelector('.slider').classList.add('slide2_background');
+    document.querySelector('.slider__bottom-line').classList.add('slider__bottom-line_visibility');
+  } else {
+    slideOrder = 0;
+    slides[0].classList.remove('slide-list'); 
+    document.querySelector('.slider').classList.remove('slide2_background');
+    document.querySelector('.slider__bottom-line').classList.remove('slider__bottom-line_visibility');
+  }
 }
